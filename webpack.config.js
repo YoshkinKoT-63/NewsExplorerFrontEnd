3e990d5489e3710dd012b483ab66bf5d300eed04
YoshkinKoT-63 <yoshkinkot63@gmail.com>
2020-10-11 22:43:40 +0400

    desktop

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -11,61 +13,71 @@ module.exports = {
     filename: '[name].[chunkhash].js'
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
         use: { loader: "babel-loader" },
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
             },
-            {
-              test: /\.css$/,
-              use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './fonts/[name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './images/[name].[ext]',
+              esModule: false
+            },
           },
           {
-            test: /\.(eot|ttf|woff|woff2)$/,
-            use: {
-              loader: 'file-loader',
-              options: {
-                name: './fonts/[name].[ext]'
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
               }
             }
           },
-          {
-            test: /\.(png|jpe?g|gif|ico|svg)$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: './images/[name].[ext]',
-                  esModule: false
-                },
-              },
-              {
-                loader: 'image-webpack-loader',
-                options: {
-                  mozjpeg: {
-                    progressive: true,
-                    quality: 65
-                  },
-                  // optipng.enabled: false will disable optipng
-                  optipng: {
-                    enabled: false,
-                  },
-                  pngquant: {
-                    quality: [0.65, 0.90],
-                    speed: 4
-                  },
-                  gifsicle: {
-                    interlaced: false,
-                  },
-                  // the webp option will enable WEBP
-                  webp: {
-                    quality: 75
-                  }
-                }
-              },
-            ]
-          }
         ]
+      }
+    ]
     },
     plugins: [
       new MiniCssExtractPlugin({
@@ -85,6 +97,9 @@ module.exports = {
         hash: true,
         template: './src/index.html',
         filename: 'index.html'
+    }),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
     new WebpackMd5Hash(),
     ],
