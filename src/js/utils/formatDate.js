08541f68cd5b5ac08b3e5b0556072407d73ac72e
YoshkinKoT-63 <yoshkinkot63@gmail.com>
2021-01-21 15:16:50 +0400

    fix format date

--- src/js/utils/formatDate.js
+++ src/js/utils/formatDate.js
@@ -1,7 +1,9 @@
 const formatDate = (date) => {
-  // console.log(date.slice(0, 2).split("-"));
-  const [year, month, day] = date.slice(0, 10).split("-");
-  return `${year}, ${month}, ${day}`;
+  const formatedDate = new Date(date);
+  return `${formatedDate.toLocaleDateString('ru-RU', {
+    day: 'numeric',
+    month: 'long',
+  })}, ${formatedDate.getFullYear()}`;
 };
 
 export default formatDate;
